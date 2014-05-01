fd540c30d87e55988b2bad7f20012513abda830b
Joonas Jauhiainen <joonas.jauhiainen@gmail.com>
2014-05-01 17:28:45 +0300

    Added better routing for creating new blogpost. Refactored code. Added security features for creating blogposts.

--- playcm/app/controllers/Application.scala
+++ playcm/app/controllers/Application.scala
@@ -9,7 +9,7 @@ import models.BlogPost
 import models.User
 
 
-object Application extends Controller {
+object Application extends Controller with Secured {
 
   val blogpostForm = Form(
     tuple(
@@ -19,14 +19,20 @@ object Application extends Controller {
   )
 
   def index = Action {
-    Ok("Hello world")
+    Ok(views.html.index(BlogPost.all(), User.all(), blogpostForm))
   }
 
   def blogposts = Action {
     Ok(views.html.index(BlogPost.all(), User.all(), blogpostForm))
   }
 
-  def newBlogpost = Action { implicit  request =>
+  def newBlogpost = IsAuthenticated { username => _ =>
+    User.findByUsername(username).map { user =>
+      Ok(views.html.blogpost.item(blogpostForm))
+    }.getOrElse(Forbidden)
+  }
+
+  def createBlogpost = IsAuthenticated { username => implicit  request =>
     blogpostForm.bindFromRequest.fold(
       errors => BadRequest(views.html.index(BlogPost.all(), User.all(), errors)),
       {
@@ -35,10 +41,6 @@ object Application extends Controller {
           Redirect(routes.Application.blogposts)
         }
       }
-      /*title => {
-        BlogPost.create(title, "some content")
-        Redirect(routes.Application.blogposts)
-      }*/
     )
 
   }
